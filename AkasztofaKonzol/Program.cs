2a4c38b2a2e29c22d1b9865ee5532d79d2458520
Adrián Medve <medveadrian03@gmail.com>
2022-11-08 14:03:05 +0100

    start of Szavak.cs

--- AkasztofaKonzol/Program.cs
+++ AkasztofaKonzol/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AkasztofaKonzol.SajatOsztaly;
 
 namespace AkasztofaKonzol
 {
     class Program
     {
+        static Szavak szotar = new Szavak();
         static void Main(string[] args)
         {
+            Console.WriteLine(szotar.SzavakSzama);
+
+            Console.ReadKey();
         }
     }
 }
--- AkasztofaKonzol/SajatOsztaly/Szavak.cs
+++ AkasztofaKonzol/SajatOsztaly/Szavak.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AkasztofaKonzol.SajatOsztaly
 {
     public class Szavak
     {
+        List<string> szos = new List<string>();
+
+        public int SzavakSzama { get; }
+
+        public Szavak()
+        {
+            SzavakFeltoltese();
+        }
+
+        private void SzavakFeltoltese()
+        {
+            
+        }
     }
 }
