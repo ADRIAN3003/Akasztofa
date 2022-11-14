812dbc202f7a23eabd3c281f3db5a45f5eca243f
Adrián Medve <medveadrian03@gmail.com>
2022-11-14 11:47:25 +0100

    create new class and move game start

--- AkasztofaKonzol/Program.cs
+++ AkasztofaKonzol/Program.cs
@@ -12,32 +12,7 @@ namespace AkasztofaKonzol
         static Szavak szotar = new Szavak();
         static void Main(string[] args)
         {
-            //Game janos = new Game("János", true);
-            Game gabor = new Game("Gábor", false);
-            string tipp;
-
-            while (!gabor.Vege)
-            {
-                Console.WriteLine($"Szóhossz: " + gabor.OsszProba);
-                Console.WriteLine($"Lehetőség: " + gabor.Lehetoseg);
-                Console.WriteLine($"Helytelen tippek: " + gabor.Helytelens);
-                Console.WriteLine($"Nem kitalált: " + gabor.NemKitalalt);
-                Console.Write("Kérek egy tippet: ");
-                tipp = Console.ReadLine();
-                Console.WriteLine("-----------------------------------------");
-                gabor.Vizsgalat(tipp);
-            }
-
-            if (gabor.Lehetoseg == 0)
-            {
-                Console.WriteLine(gabor.ToString());
-                Console.WriteLine("A játéknak vége");
-            }
-            else
-            {
-                Console.WriteLine(gabor.NemKitalalt);
-                Console.WriteLine("Sikerült kitalálni");
-            }
+            
 
             Console.ReadKey();
         }
---
+++ AkasztofaKonzol/SajatOsztaly/AkasztofaJatek.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AkasztofaKonzol.SajatOsztaly;
+
+namespace AkasztofaKonzol.SajatOsztaly
+{
+    class AkasztofaJatek
+    {
+        Game jatek;
+
+        public AkasztofaJatek(string nev, bool nehezseg)
+        {
+            jatek = new Game(nev, nehezseg);
+            
+            JatekElinditas();
+        }
+
+        private void JatekElinditas()
+        {
+            string tipp;
+
+            while (!jatek.Vege)
+            {
+                Console.WriteLine($"Szóhossz: " + jatek.OsszProba);
+                Console.WriteLine($"Lehetőség: " + jatek.Lehetoseg);
+                Console.WriteLine($"Helytelen tippek: " + jatek.Helytelens);
+                Console.WriteLine($"Nem kitalált: " + jatek.NemKitalalt);
+                Console.Write("Kérek egy tippet: ");
+                tipp = Console.ReadLine();
+                Console.WriteLine("-----------------------------------------");
+                jatek.Vizsgalat(tipp);
+            }
+
+            if (jatek.Lehetoseg == 0)
+            {
+                Console.WriteLine(jatek.ToString());
+                Console.WriteLine("A játéknak vége");
+            }
+            else
+            {
+                Console.WriteLine(jatek.NemKitalalt);
+                Console.WriteLine("Sikerült kitalálni");
+            }
+        }
+    }
+}
